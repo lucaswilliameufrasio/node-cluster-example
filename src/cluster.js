bc3b38faeab151845e047b238e71cb49fc8bd848
Lucas William <lucaswilliameufrasio@gmail.com>
2021-02-02 08:36:04 -0300

    fix: execute the freemem function

--- src/cluster.js
+++ src/cluster.js
@@ -2,13 +2,14 @@ const cluster = require('cluster')
 const worker = require('./worker')
 
 const APP_MEMORY_REQUIREMENT = 512 * 1024 * 1024 // 512MB
-const CONCURRENCY = require('os').freemem / APP_MEMORY_REQUIREMENT
+const CONCURRENCY = require('os').freemem() / APP_MEMORY_REQUIREMENT
 
 function master() {
   if (cluster.isMaster) {
     for (let index = 0; index < CONCURRENCY; index++) {
       cluster.fork()
     }
+    console.log(CONCURRENCY)
   } else {
     worker(cluster)
   }
