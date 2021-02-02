19f88c62468f0971b25a8a1c1f473d25d01d6b5c
Lucas William <lucaswilliameufrasio@gmail.com>
2021-02-02 08:23:09 -0300

    refactor: remove unnecessary code

--- src/cluster.js
+++ src/cluster.js
@@ -4,13 +4,8 @@ const worker = require('./worker')
 const APP_MEMORY_REQUIREMENT = 512 * 1024 * 1024 // 512MB
 const CONCURRENCY = require('os').freemem / APP_MEMORY_REQUIREMENT
 
-// Round-robin choosed to be used on scheduling policy
-cluster.schedulingPolicy = cluster.SCHED_RR
-
 function master() {
   if (cluster.isMaster) {
-    const cpuCount = require('os').cpus().length
-
     for (let index = 0; index < CONCURRENCY; index++) {
       cluster.fork()
     }
